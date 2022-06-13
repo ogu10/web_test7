e7c7f6e97be1d7113a16021ba746645058261ad6
ogu10 <t_ogura+4@jobins.jp>
2022-06-13 13:12:06 +0900

    save 20220613_13:12

--- OOP/oop_train2/Team.php
+++ OOP/oop_train2/Team.php
@@ -1,11 +1,15 @@
 <?php
 
 class Team {
-    public function __construct($nation, $city, $team) {
-        $this->number = $nation;
-        $this->name = $city;
-        $this->team = $team;
 
+    public $nation;
+    public $city;
+    public $team;
+
+    public function __construct($nation, $city, $team) {
+        $this->nation = $nation;
+        $this->city = $city;
+        $this->team = $team;
     }
 
     public function getTeam() {
--- OOP/oop_train2/playersData.php
+++ OOP/oop_train2/playersData.php
@@ -3,9 +3,11 @@
 require_once('common.php');
 require_once('FW.php');
 require_once('MF.php');
+require_once ('Team.php');
 
+$manU = new Team("UK","Manchester","Manchester United");
 $messi = new Forward('30', 'Messi', 'PSG', 'Argentina', 16);
-$ronald = new Forward('7', 'Ronald', 'Manchester U', 'Portuguese', 30);
+$ronald = new Forward('7', 'Ronald', $manU, 'Portuguese', 30);
 $salah = new Forward('10', 'Salah', 'Liverpool', 'Egypt', 31);
 $modric= new Midfielder('10', 'Modric', 'Real Madrid', 'Croatia', 14);
 $barella = new Midfielder('23', 'Barella', 'Inter', 'Italy', 13);
